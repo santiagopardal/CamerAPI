d3119002c1d9b0785c4b9cbb245cdc7e8791c91a
santiagopardal <santiagopardal@outlook.com>
2021-09-01 12:49:54 -0300

    removed wrong files

--- constants.js
+++
@@ -1,9 +0,0 @@
-const { readdirSync } = require('fs');
-
-VIDEOS_PATH = '../Images'
-
-function VALID_PLACES() {
-    return readdirSync(VIDEOS_PATH, { withFileTypes: true }).filter(dirent => dirent.isDirectory()).map(dirent => dirent.name)
-}
-
-module.exports = { VIDEOS_PATH, VALID_PLACES }
