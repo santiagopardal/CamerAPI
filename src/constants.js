5208561ef86dc78e1245bdcfe0c8be351bb40fea
Santiago Pardal <santiagopardal@outlook.com>
2021-09-05 23:15:33 -0300

    can properly send videos now

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const { readdirSync } = require('fs');
 
-const VIDEOS_PATH = '../Images'
+const VIDEOS_PATH = __dirname.substring(0, __dirname.length - 13) + '/Images'
 
 function VALID_PLACES() {
     return readdirSync(VIDEOS_PATH, { withFileTypes: true }).filter(dirent => dirent.isDirectory()).map(dirent => dirent.name)
--- src/routes/videos.js
+++ src/routes/videos.js
@@ -68,50 +68,11 @@ router.get('/:camera/', async (request, response, next) => {
     }
 })
 
-router.get('/:camera/stream/:date', async (request, response, next) => {
-    try {
-        const pth = validateCameraAndDate(request.params.camera, request.params.date)
-
-        const stat = statSync(pth)
-        const fileSize = stat.size
-        const range = request.headers.range
-        
-        if (range) {
-            const parts = range.replace(/bytes=/, "").split("-")
-            const start = parseInt(parts[0], 10)
-            const end = parts[1] ? parseInt(parts[1], 10) : fileSize-1
-            const chunkSize = (end-start)+1
-            
-            const file = createReadStream(pth, {start, end})
-            
-            const head = {
-                'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-                'Accept-Ranges': 'bytes',
-                'Content-Length': chunkSize,
-                'Content-Type': 'video/mp4'
-            }
-
-            response.writeHead(206, head);
-            file.pipe(response);
-        } else {
-            const head = {
-                'Content-Length': fileSize,
-                'Content-Type': 'video/mp4',
-            }
-
-            response.writeHead(200, head)
-            createReadStream(pth).pipe(response)
-        }
-    } catch (e) {
-        next(e)
-    }
-})
-
 router.get('/:camera/download/:date', async (request, response, next) => {
     try {
         const pth = validateCameraAndDate(request.params.camera, request.params.date)
-
-        response.download(pth)
+        
+        response.sendFile(pth)
     } catch (e) {
         next(e)
     }
