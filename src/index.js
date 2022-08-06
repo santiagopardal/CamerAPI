65a1fa9a3c679f605e6c47f8f294e7626f2136d4
Santiago Pardal <santiagopardal@outlook.com>
2022-08-06 15:31:56 -0300

    add cron job instead of scheduling myself, also add new endpoint for temporal videos

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const app = express()
 app.set('json spaces', 2)
 
 const cameras = require('./routes/cameras')
+const temporal_videos = require('./routes/temporal_videos')
 
 app.use(morgan('dev'))
 app.use(express.urlencoded({extended: false, limit: '5mb'}))
@@ -13,6 +14,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use('/api/cameras/', cameras)
+app.use('/api/temporal_videos/', temporal_videos)
 
 app.use((req, res, next) => {
     const err = new Error('Url not found');
