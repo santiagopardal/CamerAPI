800a05533b87badc0408819f5c70fb2b22397e14
Santiago Pardal <santiagopardal@outlook.com>
2023-11-04 17:06:48 -0300

    bugfix

--- src/models/dao/knex.js
+++ src/models/dao/knex.js
@@ -1,6 +1,5 @@
 const knex = require('knex')
 const options = require('../../../knexfile')
-
-const knexConnection = knex(options[process.env.NODE_ENV])
+const knexConnection = knex(options[process.env.NODE_ENV ?? 'development'])
 
 module.exports = knexConnection;
