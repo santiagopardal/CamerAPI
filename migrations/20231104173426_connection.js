c5ce639e20a4cde7bc083c7c2e9097c317f56406
Santiago Pardal <santiagopardal@outlook.com>
2023-11-05 19:28:40 -0300

    fixes

--- migrations/20231104173426_connection.js
+++ migrations/20231104173426_connection.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
       table.increments('id', {primaryKey: true})
       table.integer('camera').unsigned().notNullable().references('id').inTable('camera').onDelete('CASCADE')
       table.string('message').notNullable()
-      table.date('date').notNullable()
+      table.datetime('date').notNullable()
       table.foreign('camera', 'FK_camera_camera')
   })
 };
