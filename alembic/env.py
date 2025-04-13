655111dc69b18495b8b95696f2549df12fad4792
Santiago Pardal <santiagopardal@outlook.com>
2025-04-13 16:28:45 -0300

    Fix migrations

--- alembic/env.py
+++ alembic/env.py
@@ -67,9 +67,11 @@ async def run_async_migrations() -> None:
     and associate a connection with the context.
 
     """
+    configuration = config.get_section(config.config_ini_section)
+    configuration["sqlalchemy.url"] = settings.get_settings().SQLALCHEMY_DATABASE_URI
 
     connectable = async_engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        configuration,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
